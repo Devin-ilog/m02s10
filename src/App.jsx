344ca7ab53f27163bea900c733f71a836e36576b
Thais Bertoldo <thaisbertoldo11@gmail.com>
2023-03-20 20:31:24 -0300

    aula01 parte 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import FormControlado from './components/FormControlado';
+
+function App() {
+  return (
+    <div className='App'>
+      <FormControlado />
+    </div>
+  );
+}
+
+export default App;
